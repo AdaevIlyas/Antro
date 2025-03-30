d0d31482844afc5f05e9add169ffe3185c7d9ea7
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2025-03-31 03:02:02 +0600

    audit mobile

--- src/js/components/auditBlock.js
+++ src/js/components/auditBlock.js
@@ -6,6 +6,8 @@ export const auditBlock = () => {
     
     const auditSart1 = document.querySelector('.js-audit-start-1');
     const auditSart2 = document.querySelector('.js-audit-start-2');
+    const auditSartImg1 = document.querySelector('.js-audit-start-img-1');
+    const auditSartImg2 = document.querySelector('.js-audit-start-img-2');
 
     if ( auditSart1 && auditSart2 ) {
         
@@ -13,13 +15,20 @@ export const auditBlock = () => {
             
             let rect = auditSart2.getBoundingClientRect();
 
-            if ( rect.top < 600 ) {
-                auditSart1.classList.add('audit-start__block_gray');
-                auditSart2.classList.remove('audit-start__block_gray');
-            } else {
-                auditSart2.classList.add('audit-start__block_gray');
-                auditSart1.classList.remove('audit-start__block_gray');
+            if ( window.innerWidth > 480 ) {
+                if ( rect.top < 600 ) {
+                    auditSart1.classList.add('audit-start__block_gray');
+                    auditSart2.classList.remove('audit-start__block_gray');
+                    auditSartImg1.style.opacity = '0';
+                    auditSartImg2.style.opacity = '1';
+                } else {
+                    auditSart2.classList.add('audit-start__block_gray');
+                    auditSart1.classList.remove('audit-start__block_gray');
+                    auditSartImg1.style.opacity = '1';
+                    auditSartImg2.style.opacity = '0';
+                }
             }
+            
 
         });
         
