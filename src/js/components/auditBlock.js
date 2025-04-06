d9bbb7dcabbe775e4b57e1d7c6f4a26dde602dc9
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2025-04-06 19:39:32 +0600

    fixes

--- src/js/components/auditBlock.js
+++ src/js/components/auditBlock.js
@@ -4,28 +4,28 @@
 export const auditBlock = () => {
 
     
-    const auditSart1 = document.querySelector('.js-audit-start-1');
-    const auditSart2 = document.querySelector('.js-audit-start-2');
-    const auditSartImg1 = document.querySelector('.js-audit-start-img-1');
-    const auditSartImg2 = document.querySelector('.js-audit-start-img-2');
+    const auditStart1 = document.querySelector('.js-audit-start-1');
+    const auditStart2 = document.querySelector('.js-audit-start-2');
+    const auditStartImg1 = document.querySelector('.js-audit-start-img-1');
+    const auditStartImg2 = document.querySelector('.js-audit-start-img-2');
 
-    if ( auditSart1 && auditSart2 ) {
+    if ( auditStart1 && auditStart2 ) {
         
         document.addEventListener('scroll',function(){
             
-            let rect = auditSart2.getBoundingClientRect();
+            let rect = auditStart2.getBoundingClientRect();
 
             if ( window.innerWidth > 480 ) {
-                if ( rect.top < 600 ) {
-                    auditSart1.classList.add('audit-start__block_gray');
-                    auditSart2.classList.remove('audit-start__block_gray');
-                    auditSartImg1.style.opacity = '0';
-                    auditSartImg2.style.opacity = '1';
+                if ( rect.top < 500 ) {
+                    auditStart1.classList.add('audit-start__block_gray');
+                    auditStart2.classList.remove('audit-start__block_gray');
+                    auditStartImg1.style.opacity = '0';
+                    auditStartImg2.style.opacity = '1';
                 } else {
-                    auditSart2.classList.add('audit-start__block_gray');
-                    auditSart1.classList.remove('audit-start__block_gray');
-                    auditSartImg1.style.opacity = '1';
-                    auditSartImg2.style.opacity = '0';
+                    auditStart2.classList.add('audit-start__block_gray');
+                    auditStart1.classList.remove('audit-start__block_gray');
+                    auditStartImg1.style.opacity = '1';
+                    auditStartImg2.style.opacity = '0';
                 }
             }
             
